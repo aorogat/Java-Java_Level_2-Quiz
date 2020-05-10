0273a89a1bd927d5cd826e2ab8a7f8e6e7e5dbd8
Abdelghny Orogat <53449461+aorogat@users.noreply.github.com>
2020-05-10 00:21:07 -0400

    Update q2.java

--- q2.java
+++ q2.java
@@ -1,5 +1,4 @@
 package shapes;
-
 public class Circle 
 {
     protected double radius;
@@ -7,7 +6,6 @@ public class Circle
     public Circle(double radius) {
         this.radius = radius;
     }
-
     
     public double getArea()
     {
@@ -21,7 +19,50 @@ public class Circle
     public void setRadius(double radius) {
         this.radius = radius;
     }
+}
+//////////////////////////////////////////////////////////////////////////
+
+package shapes;
+
+public class Cylinder
+{
+    private double h;
+    Circle c;
+
+    public Cylinder(double h, double radius) {
+        c = new Circle(radius);
+        this.h = h;
+    }
     
+    public double getVolume()
+    {
+        return c.getArea()*h;
+    }
     
+    public double getH() {
+        return h;
+    }
+
+    public void setH(double h) {
+        this.h = h;
+    }
+}
+///////////////////////////////////////////////////////////////////////////////
+package mathshapes;
+
+import shapes.Cylinder;
+import shapes.Circle;
+
+public class MathShapes {
+
+    public static void main(String[] args) 
+    {
+        Circle c1 = new Circle(4);
+        Cylinder cy = new Cylinder(7.3, 2);
+        
+        System.out.println(c1.getArea());
+        System.out.println(cy.getVolume());
+    }
     
 }
+
